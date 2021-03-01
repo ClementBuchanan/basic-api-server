e0270f0cfda266f6daa4209b5f4834fce4c05800
Clement Buchanan <clement.buchanan@gmail.com>
2021-02-28 23:33:37 -0800

    2nd push

--- src/models/thing.js
+++ src/models/thing.js
@@ -1,8 +1,45 @@
 'use strict';
 
-//this will be an in-memory database model
-let db = {};
+//Class syntax has uppercase and provides us woth structure and readibility
+// this is an inline data model
+class ThingsModel {
+    constructor() {
+        this.id = 0;
+        this.db = []
+    }
+    // READ == will work in 2 ways for returning all items in the db or a specific item in the db
+    // if GET is empty it will bring back everything from the db. If it has something in it, it will bring back that specific item
+    // In here we are making routes with db interactions in them
+    get(id) {
+        if(id) {
+            return this.db.find(record => record.id === id);
+        } else {
+            return this.db;
+        }
+    }
 
+    // CREATE === When we create something we increment (++id) the id, hold the thing given to the db item, then push it up to the db
+    create(db) {
+        let record = {
+            id = ++this.id,
+            record: obj
+        }
+        this.db.push(record);
+        return record;
+    }
 
-db.person = 'Clement';
-db.kids = ['Thadddeus', 'Abraham', 'Brnjamin', 'Cameron']
+    // UPDATE
+    update(id, obj) {
+        if(id) {
+            return obj
+        }
+    }
+    // DELETE
+    delete(id) {
+        if(id) {
+            return null;
+        }
+    }
+}
+
+module.exports = ThingsModel;
